cfcc46dbc2ad60110f707fd150ef5e48dd8d1075
Matheus Santos <matheuss.tecnico@gmail.com>
2020-11-24 19:54:18 -0300

    novo codigo

---
+++ ApiProducts/Controllers/ProductsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiProducts.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProducts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Products> list = new List<Products>();
+
+        public ProductsController()
+        {
+           
+            list.Add(new Products(1, "Arroz", 50));
+            list.Add(new Products(2, "Feijao", 70));
+            list.Add(new Products(3, "Macarrao", 80));
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<Products>> Get()
+        {
+            return Ok(list);
+
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] Products products)
+        {
+            list.Add(products);
+            return Created("Products/Post", list);
+        }
+        
+
+        
+        [HttpDelete("{code}")]
+        public ActionResult Delete([FromQuery] int code)
+        {
+            list.RemoveAt(list.IndexOf(list.First(x => x.Code.Equals(code))));
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Products products)
+        {
+            int id = list.IndexOf(list.First(x => x.Code.Equals(products.Code)));
+
+            list[id].Code = products.Code;
+            list[id].Description = products.Description;
+            list[id].Stock = products.Stock;
+
+            return Ok();
+
+        }
+        [HttpPatch]
+        public ActionResult Patch([FromBody] Products products)
+        {
+            int id = list.IndexOf(list.First(x => x.Code.Equals(products.Code)));
+
+            list[id].Code = products.Code;
+            list[id].Description = products.Description;
+            list[id].Stock = products.Stock;
+
+            return Ok();
+
+        }
+
+    }
+}
---
+++ ApiProducts/Models/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProducts.Models
+{
+    public class Products
+    {
+        public int Code { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+
+        public Products(int code, string description, int stock)
+        {
+            Code = code;
+            Description = description;
+            Stock = stock;
+        }
+        public Products() { 
+            
+        }
+
+
+    }
+}
